86682123bae5213720e5169d504736cb36afd3f6
Graham Strickland <grahamhstrickland@gmail.com>
2022-05-27 18:57:36 +0200

    Update output format and function comparison

--- ch01/src/p1_1/CompareRunningTimes.java
+++ ch01/src/p1_1/CompareRunningTimes.java
@@ -5,8 +5,8 @@ import java.lang.Math.*;
 public class CompareRunningTimes {
 	static double runtimes[] = {1.0, 6.0E1, 3.6E3, 8.64E4, 2.592E6, 3.1536E7, 3.1536E9};
 	
-	public static int factorial(int n) {
-		int fact = n, multiplier = n;
+	public static double factorial(double n) {
+		int fact = (int)n, multiplier = (int)n;
 		while (multiplier > 0) {
 			fact = fact * multiplier;
 			multiplier--;
@@ -17,75 +17,51 @@ public class CompareRunningTimes {
  	
 	public static void printRuntimes() {
 		// lg(n) time
-		System.out.print("n\t");
-		for (double time : runtimes) {
-			System.out.print((Math.log(time) / Math.log(2)) * 1E6);
-			System.out.print("\t\t");
-		}
+		System.out.print("lg(n)\t\t");
+		for (double time : runtimes)
+			System.out.format("%.5e\t\t", time / ((Math.log(1E-6) / Math.log(2))));
 		System.out.print('\n');
 		
 		// sqrt(n) time
-		System.out.print("n\t");
-		for (double time : runtimes) {
-			System.out.print(Math.sqrt(time) * 1E6);
-			System.out.print("\t\t");
-		}
+		System.out.print("sqrt(n)\t\t");
+		for (double time : runtimes)
+			System.out.format("%.5e\t\t", time / Math.sqrt(1E-6));
 		System.out.print('\n');
 		
-		// constant time
-		System.out.print("n\t");
-		for (double time : runtimes) {
-			System.out.print(time * 1E6);
-			System.out.print("\t\t");
-		}
+		// linear time
+		System.out.print("n\t\t");
+		for (double time : runtimes)
+			System.out.format("%.5e\t\t", time / 1E-6);
 		System.out.print('\n');
 		
 		// n(lg(n)) time
-		System.out.print("n\t");
-		for (double time : runtimes) {
-			System.out.print(time * (Math.log(time) / Math.log(2)) * 1E6);
-			System.out.print("\t\t");
-		}
-		System.out.print('\n');
-				
-		// linear time
-		System.out.print("n\t");
-		for (double time : runtimes) {
-			System.out.print(time * 1E6);
-			System.out.print("\t\t");
-		}
+		System.out.print("n(lg(n))\t");
+		for (double time : runtimes)
+			System.out.format("%.5e\t\t", time / (1E-6 * (Math.log(1E-6) / Math.log(2))));
 		System.out.print('\n');
 		
 		// quadratic time
-		System.out.print("n\t");
-		for (double time : runtimes) {
-			System.out.print(Math.pow(time, 2) * 1E6);
-			System.out.print("\t\t");
-		}
+		System.out.print("n^2\t\t");
+		for (double time : runtimes)
+			System.out.format("%.5e\t\t", time / Math.pow(1E-6, 2));
 		System.out.print('\n');
 		
 		// cubic time
-		System.out.print("n\t");
-		for (double time : runtimes) {
-			System.out.print(Math.pow(time, 3) * 1E6);
-			System.out.print("\t\t");
-		}
+		System.out.print("n^3\t\t");
+		for (double time : runtimes)
+			System.out.format("%.5e\t\t", time / Math.pow(1E-6, 3));
 		System.out.print('\n');
 		
 		// 2^n time
-		System.out.print("n\t");
-		for (double time : runtimes) {
-			System.out.print(Math.pow(2, time) * 1E6);
-			System.out.print("\t\t");
-		}
+		System.out.print("2^n\t\t");
+		for (double time : runtimes)
+			System.out.format("%.5e\t\t", time / Math.pow(2, 1E-6));
 		System.out.print('\n');
 		
 		// n! time
-		System.out.print("n\t");
-		for (double time : runtimes) {
-			System.out.print(factorial((int)time) * 1E6);
-			System.out.print("\t\t");
-		}
+		System.out.print("n!\t\t");
+		for (double time : runtimes)
+			System.out.format("%.5e\t\t", time / factorial(1E-6));
 		System.out.print('\n');
 	}
 	
@@ -93,10 +69,11 @@ public class CompareRunningTimes {
 		System.out.print("Comparison of running times for the largest problem size n of a problem,");
 		System.out.print("that can be solved in time t,\nassuming that the algorithm to solve the problem");
 		System.out.print("takes f(n) microseconds:\n\n---------------------------------------------------");
-		System.out.print("-----------------------------------------------------------------------------\n");
-		System.out.print("f(n)\t1 second\t1 minute\t1 hour\t\t1 day\t\t1 month\t\t1 year\t\t1 century\n");
-		System.out.print("_______________________________________________________________________________");
-		System.out.print("_________________________________________________\n");
+		System.out.print("-------------------------------------------------------------------------------------------");
+		System.out.print("------------------------------\n");
+		System.out.print("f(n)\t\t1 second\t\t1 minute\t\t1 hour\t\t\t1 day\t\t\t1 month\t\t\t1 year\t\t\t1 century\n");
+		System.out.print("___________________________________________________________________________________________");
+		System.out.print("_________________________________________________________________________________\n");
 		printRuntimes();
 	}
 }
