595ca41bad45d6381d022ff3803ef371964225cf
Graham Strickland <grahamhstrickland@gmail.com>
2023-08-04 19:55:29 +0200

    Add Merge-Sort with no sentinel algorithm

---
+++ ch02/section2_3/mergesortnosentinel.cpp
@@ -0,0 +1,82 @@
+// Merge-Sort algorithm without sentinel values from ex. 2.3-2 p.37 of CLRS 3e
+
+#include <iostream>
+
+void merge(int A[], int p, int q, int r);
+void mergeSort(int A[], int p, int r);
+void testMergeSort(int A[], int len);
+
+int main(int argc, char *argv[]) {
+    int A[] = {5, 2, 4, 6, 1, 3};
+    int B[] = {31, 41, 59, 26, 41, 58};
+    int C[] = {6, 5, 4, 3, 2, 1};
+    int D[] = {5, 2, 4, 7, 1, 3, 2, 6};
+    int E[] = {3, 41, 52, 26, 38, 57, 9, 49};
+
+    int len = 6;
+    testMergeSort(A, len);
+    testMergeSort(B, len);
+    testMergeSort(C, len);
+
+    len = 8;
+    testMergeSort(D, len);
+    testMergeSort(E, len);
+
+    return EXIT_SUCCESS;
+}
+
+void merge(int A[], int p, int q, int r) {
+    int n1 = q - p + 1, n2 = r - q;
+    
+    int L[n1], R[n2];
+    
+    for (int i = 0; i < n1; i++)
+        L[i] = A[p+i];
+    for (int j = 0; j < n2; j++)
+        R[j] = A[q+j+1];
+    
+    int i = 0, j = 0;
+    
+    for (int k = p; k <= r; k++) {
+        if (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                A[k] = L[i];
+                i++;
+            } else {
+                A[k] = R[j];
+                j++;
+            }
+        } else {
+            if (i < n1) {
+                A[k] = L[i];
+                i++;
+            } else if (j < n2) {
+                A[k] = R[j];
+                j++;
+            }
+        }
+    }
+}
+
+void mergeSort(int A[], int p, int r) {
+    if (p < r) {
+        int q = (p + r) / 2;
+        mergeSort(A, p, q);
+        mergeSort(A, q+1, r);
+        merge(A, p, q, r);
+    }
+}
+
+void testMergeSort(int A[], int len) {
+    std::cout << "Array before Merge-Sort: ";
+    for (int i = 0; i < len; i++)
+        std::cout << A[i] << ' ';
+    std::cout << '\n';
+    
+    mergeSort(A, 0, len-1);
+    
+    std::cout << "Array after Merge-Sort: ";
+    for (int i = 0; i < len; i++)
+        std::cout << A[i] << ' ';
+    std::cout << "\n\n";
+}
