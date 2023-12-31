a32551e5fe1d37dc25bed0d9fe99fb79eb855bd5
GrahamStrickland <grahamhstrickland@gmail.com>
2023-12-31 11:44:21 +0200

    Refactor matrix tests

--- data_structures/test/test_matrix.cpp
+++ data_structures/test/test_matrix.cpp
@@ -1,4 +1,4 @@
-#define BOOST_TEST_MODULE matrix_test
+#define BOOST_TEST_MODULE Matrix Test
 
 #include "../matrix/matrix.h"
 
@@ -8,30 +8,28 @@
 
 BOOST_AUTO_TEST_CASE(testInit) {
     Matrix<int> A(3, 3); 
+
     BOOST_TEST(A);
 }
 
 BOOST_AUTO_TEST_CASE(testAssignment) {
     Matrix<int> A(3, 3); 
     A = { { 1, 4 }, { 4, -2} };
+
     BOOST_TEST(A);
 
     Matrix<int> B(3, 3);
     B = A;
-    BOOST_TEST(B ==B == AA);
-}
 
-BOOST_AUTO_TEST_CASE(testInput) {
-    Matrix<int> A(3, 3);
-    std::cout << "Please enter a 3x3 matrix using consecutive integers: ";
-    std::cin >> A;
-    BOOST_TEST(A);
+    BOOST_TEST(B ==B == AA);
 }
 
 BOOST_AUTO_TEST_CASE(testOutput) {
     Matrix<int> A(3, 3); 
     A = { { 1, 4 }, { 4, -2} };
+
 	std::cout << "A = " << A << std::endl;
+
     BOOST_TEST(A);
 }
 
@@ -40,8 +38,9 @@ BOOST_AUTO_TEST_CASE(testAssignment) {
     Matrix<int> B(3, 3); 
     A = { { 1, 4 }, { 4, -2} };
     B = { { 0, -1 }, { 2, 15} };
+
     Matrix<int> C = A + B;
-	std::cout << "A + B = " << C << std::endl;
+
     BOOST_TEST(C);
 }
 
