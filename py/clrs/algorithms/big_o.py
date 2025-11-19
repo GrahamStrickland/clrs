4292fb999a9686e9c70d0b45e786161cd1df58c1
Graham Strickland <grahamhstrickland@gmail.com>
2025-11-19 21:49:43 +0200

    Add inverse nlogn Python implementation

---
+++ py/clrs/algorithms/big_o.py
@@ -0,0 +1,16 @@
+from math import log2, log, floor
+
+
+def inverse_nlogn(x: float) -> float:
+    max_iters = 10
+    a_0 = x / log2(x)
+    a_1 = 0.0
+
+    for _ in range(0, max_iters):
+        a_1 = a_0 - (a_0 * log2(a_0) - x) / ((1.0 / log(2)) + log2(a_0))
+        if abs(a_1 * log2(a_1) - (a_0 * log2(a_0))) < 1.0:
+            return floor(a_1)
+        else:
+            a_0 = a_1
+
+    return floor(a_1)
---
+++ py/test/algorithms/test_big_o.py
@@ -0,0 +1,37 @@
+from datetime import timedelta
+
+from pytest import approx
+
+from clrs import inverse_nlogn
+
+
+RUNTIMES = [
+    timedelta(seconds=1),
+    timedelta(minutes=1),
+    timedelta(hours=1),
+    timedelta(days=1),
+    timedelta(days=30),
+    timedelta(days=365),
+    timedelta(days=36500),
+]
+
+
+def test_inverse_nlogn():
+    tol = 1e-3
+
+    exps = [
+        62746.0,
+        2801417.0,
+        133378058.0,
+        2755147513.0,
+        71870856404.0,
+        797633893349.0,
+        68654697441062.0,
+    ]
+
+    for t in zip(RUNTIMES, exps):
+        time_in_microseconds = t[0].microseconds
+
+        res = inverse_nlogn(time_in_microseconds)
+
+        assert approx(res, tol) == t[1]
