7f6c53e442921f6c9ad25ce9bedf1dabbc5ec2ac
Graham Strickland <grahamhstrickland@gmail.com>
2025-12-21 14:33:10 +0200

    Add more traits to Rust Matrix<T> struct and test

--- rs/clrs_data_structures/src/lib.rs
+++ rs/clrs_data_structures/src/lib.rs
@@ -6,20 +6,38 @@ mod tests {
     use super::matrix::Matrix;
 
     #[test]
-    fn test_matrix() {
-        let a: Matrix<i32> = Matrix::new(2, 2);
+    fn test_matrix_eq() {
+        let a: Matrix<i32> = Matrix::new(3, 3, vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]]);
+        let mut b = a.clone();
+
+        assert_eq!(a, b);
+
+        b = Matrix::new(3, 3, vec![vec![1, 1, 1], vec![1, 1, 1], vec![1, 1, 1]]);
+        assert_ne!(a, b);
     }
 
     #[test]
-    fn test_add() {
-        let a: Matrix<i32> = Matrix::new(2, 2);
-        let b: Matrix<i32> = Matrix::new(2, 2);
+    fn test_matrix_add() {
+        let a: Matrix<i32> = Matrix::new(3, 3, vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]]);
+        let b: Matrix<i32> =
+            Matrix::new(3, 3, vec![vec![1, -1, 1], vec![-1, 1, -1], vec![1, -1, 1]]);
 
         let c = a + b;
+        assert_eq!(
+            c,
+            Matrix::new(3, 3, vec![vec![2, 1, 4], vec![3, 6, 5], vec![8, 7, 10]])
+        );
+    }
 
-        let d: Matrix<f64> = Matrix::new(2, 2);
-        let e: Matrix<f64> = Matrix::new(2, 2);
-
-        let f = d + e;
+    #[test]
+    fn test_matrix_sub() {
+        let a: Matrix<i32> = Matrix::new(3, 3, vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]]);
+        let b: Matrix<i32> =
+            Matrix::new(3, 3, vec![vec![1, -1, 1], vec![-1, 1, -1], vec![1, -1, 1]]);
+        let c = a - b;
+        assert_eq!(
+            c,
+            Matrix::new(3, 3, vec![vec![0, 3, 2], vec![5, 4, 7], vec![6, 9, 8]])
+        );
     }
 }
--- rs/clrs_data_structures/src/matrix.rs
+++ rs/clrs_data_structures/src/matrix.rs
@@ -1,32 +1,102 @@
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 use std::ops;
 
 use num::traits::Zero;
 
 pub struct Matrix<T>
 where
-    T: Zero + Sized + Clone + Copy,
+    T: Zero + ops::Sub<Output = T> + Sized + Clone + Copy + Display + PartialEq,
 {
-    rows: usize,
-    cols: usize,
+    pub rows: usize,
+    pub cols: usize,
     data: Vec<T>,
 }
 
 impl<T> Matrix<T>
 where
-    T: Zero + Sized + Clone + Copy,
+    T: Zero + ops::Sub<Output = T> + Sized + Clone + Copy + Display + PartialEq,
 {
-    pub fn new(cols: usize, rows: usize) -> Matrix<T> {
+    pub fn new(cols: usize, rows: usize, data: Vec<Vec<T>>) -> Matrix<T> {
+        assert_eq!(data.len(), rows);
+        let mut new_data = vec![T::zero(); rows * cols];
+
+        for row in 0..data.len() {
+            assert_eq!(data[row].len(), cols);
+            for col in 0..cols {
+                new_data[row * cols + col] = data[row][col];
+            }
+        }
+
         Matrix {
             rows: rows,
             cols: cols,
-            data: vec![T::zero(); rows * cols],
+            data: new_data,
         }
+    }
+}
+
+impl<T> Clone for Matrix<T>
+where
+    T: Zero + ops::Sub<Output = T> + Sized + Clone + Copy + Display + PartialEq,
+{
+    fn clone(&self) -> Matrix<T> {
+        Matrix {
+            rows: self.rows,
+            cols: self.cols,
+            data: self.data.clone(),
+        }
+    }
+}
+
+impl<T> Debug for Matrix<T>
+where
+    T: Zero + ops::Sub<Output = T> + Sized + Clone + Copy + Display + PartialEq,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "\n[")?;
+        for row in 0..self.rows {
+            for col in 0..self.cols {
+                if col == 0 {
+                    write!(f, "[")?;
+                } else {
+                    write!(f, ", ")?;
+                }
+                write!(f, "{}", self.data[row * self.cols + col])?;
+            }
+
+            write!(f, "]")?;
+            if row < self.rows - 1 {
+                write!(f, ",\n")?;
+            }
+        }
+        write!(f, "]\n")?;
+
+        FmtResult::Ok(())
+    }
+}
+
+impl<T> PartialEq for Matrix<T>
+where
+    T: Zero + ops::Sub<Output = T> + Sized + Clone + Copy + Display + PartialEq,
+{
+    fn eq(&self, _rhs: &Matrix<T>) -> bool {
+        if self.rows != _rhs.rows || self.cols != _rhs.cols {
+            return false;
+        }
+
+        for i in 0..self.data.len() {
+            if self.data[i] != _rhs.data[i] {
+                return false;
+            }
+        }
+
+        true
     }
 }
 
 impl<T> ops::Add<Matrix<T>> for Matrix<T>
 where
-    T: Zero + Sized + Clone + Copy,
+    T: Zero + ops::Sub<Output = T> + Sized + Clone + Copy + Display + PartialEq,
 {
     type Output = Matrix<T>;
 
@@ -37,6 +107,29 @@ where
         let mut data = vec![T::zero(); self.rows * self.cols];
         for i in 0..self.data.len() {
             data[i] = self.data[i] + _rhs.data[i];
+        }
+
+        Matrix {
+            rows: self.rows,
+            cols: self.cols,
+            data: data,
+        }
+    }
+}
+
+impl<T> ops::Sub<Matrix<T>> for Matrix<T>
+where
+    T: Zero + ops::Sub<Output = T> + Sized + Clone + Copy + Display + PartialEq,
+{
+    type Output = Matrix<T>;
+
+    fn sub(self, _rhs: Matrix<T>) -> Matrix<T> {
+        assert_eq!(self.rows, _rhs.rows);
+        assert_eq!(self.cols, _rhs.cols);
+
+        let mut data = vec![T::zero(); self.rows * self.cols];
+        for i in 0..self.data.len() {
+            data[i] = self.data[i] - _rhs.data[i];
         }
 
         Matrix {
