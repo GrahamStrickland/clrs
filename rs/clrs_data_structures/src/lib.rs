6403674e37aa3560f1cadd6b2ca0fa5def730589
Graham Strickland <grahamhstrickland@gmail.com>
2026-02-21 21:30:20 +0200

    Add Rust square matrix implementation

--- rs/clrs_data_structures/src/lib.rs
+++ rs/clrs_data_structures/src/lib.rs
@@ -6,6 +6,21 @@ mod tests {
     use super::matrix::Matrix;
 
     #[test]
+    fn test_matrix_index() {
+        let a: Matrix<i32> = Matrix::new(3, 3, vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]]);
+
+        assert_eq!(a[0][0], 1);
+        assert_eq!(a[0][1], 2);
+        assert_eq!(a[0][2], 3);
+        assert_eq!(a[1][0], 4);
+        assert_eq!(a[1][1], 5);
+        assert_eq!(a[1][2], 6);
+        assert_eq!(a[2][0], 7);
+        assert_eq!(a[2][1], 8);
+        assert_eq!(a[2][2], 9);
+    }
+
+    #[test]
     fn test_matrix_eq() {
         let a: Matrix<i32> = Matrix::new(3, 3, vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]]);
         let mut b = a.clone();
@@ -59,6 +74,114 @@ mod tests {
         assert_eq!(
             f,
             Matrix::new(2, 2, vec![vec![0.0, -0.75], vec![-0.75, 0.0]])
+        );
+    }
+
+    // Square matrix tests
+    use super::matrix::SquareMatrix;
+
+    #[test]
+    fn test_square_matrix_index() {
+        let a: SquareMatrix<i32> =
+            SquareMatrix::new(3, vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]]);
+
+        assert_eq!(a[0][0], 1);
+        assert_eq!(a[0][1], 2);
+        assert_eq!(a[0][2], 3);
+        assert_eq!(a[1][0], 4);
+        assert_eq!(a[1][1], 5);
+        assert_eq!(a[1][2], 6);
+        assert_eq!(a[2][0], 7);
+        assert_eq!(a[2][1], 8);
+        assert_eq!(a[2][2], 9);
+    }
+
+    #[test]
+    fn test_square_matrix_submatrix() {
+        let a: SquareMatrix<i32> = SquareMatrix::new(2, vec![vec![1, 2], vec![3, 4]]);
+
+        assert_eq!(a.submatrix(0, 0), SquareMatrix::new(1, vec![vec![1]]));
+        assert_eq!(a.submatrix(0, 1), SquareMatrix::new(1, vec![vec![2]]));
+        assert_eq!(a.submatrix(1, 0), SquareMatrix::new(1, vec![vec![3]]));
+        assert_eq!(a.submatrix(1, 1), SquareMatrix::new(1, vec![vec![4]]));
+
+        let b: SquareMatrix<i32> = SquareMatrix::new(
+            4,
+            vec![
+                vec![1, 2, 3, 4],
+                vec![5, 6, 7, 8],
+                vec![9, 10, 11, 12],
+                vec![13, 14, 15, 16],
+            ],
+        );
+
+        assert_eq!(
+            b.submatrix(0, 0),
+            SquareMatrix::new(2, vec![vec![1, 2], vec![5, 6]])
+        );
+        assert_eq!(
+            b.submatrix(0, 1),
+            SquareMatrix::new(2, vec![vec![3, 4], vec![7, 8]])
+        );
+        assert_eq!(
+            b.submatrix(1, 0),
+            SquareMatrix::new(2, vec![vec![9, 10], vec![13, 14]])
+        );
+        assert_eq!(
+            b.submatrix(1, 1),
+            SquareMatrix::new(2, vec![vec![11, 12], vec![15, 16]])
+        );
+    }
+
+    #[test]
+    fn test_square_matrix_eq() {
+        let a: SquareMatrix<i32> =
+            SquareMatrix::new(3, vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]]);
+        let mut b = a.clone();
+
+        assert_eq!(a, b);
+
+        b = SquareMatrix::new(3, vec![vec![1, 1, 1], vec![1, 1, 1], vec![1, 1, 1]]);
+        assert_ne!(a, b);
+    }
+
+    #[test]
+    fn test_square_matrix_add() {
+        let a: SquareMatrix<i32> =
+            SquareMatrix::new(3, vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]]);
+        let b: SquareMatrix<i32> =
+            SquareMatrix::new(3, vec![vec![1, -1, 1], vec![-1, 1, -1], vec![1, -1, 1]]);
+
+        let c = a + b;
+        assert_eq!(
+            c,
+            SquareMatrix::new(3, vec![vec![2, 1, 4], vec![3, 6, 5], vec![8, 7, 10]])
+        );
+    }
+
+    #[test]
+    fn test_square_matrix_sub() {
+        let a: SquareMatrix<i32> =
+            SquareMatrix::new(3, vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]]);
+        let b: SquareMatrix<i32> =
+            SquareMatrix::new(3, vec![vec![1, -1, 1], vec![-1, 1, -1], vec![1, -1, 1]]);
+        let c = a - b;
+        assert_eq!(
+            c,
+            SquareMatrix::new(3, vec![vec![0, 3, 2], vec![5, 4, 7], vec![6, 9, 8]])
+        );
+    }
+
+    #[test]
+    fn test_square_matrix_mul() {
+        let a: SquareMatrix<i32> =
+            SquareMatrix::new(3, vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]]);
+        let b: SquareMatrix<i32> =
+            SquareMatrix::new(3, vec![vec![1, -1, 1], vec![-1, 1, -1], vec![1, -1, 1]]);
+        let c = a * b;
+        assert_eq!(
+            c,
+            SquareMatrix::new(3, vec![vec![2, -2, 2], vec![5, -5, 5], vec![8, -8, 8]])
         );
     }
 }
--- rs/clrs_data_structures/src/matrix.rs
+++ rs/clrs_data_structures/src/matrix.rs
@@ -81,23 +81,27 @@ where
         + PartialEq,
 {
     fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
-        write!(f, "\n[")?;
-        for row in 0..self.rows {
-            for col in 0..self.cols {
-                if col == 0 {
-                    write!(f, "[")?;
-                } else {
-                    write!(f, ", ")?;
+        if self.rows == 1 && self.cols == 1 {
+            writeln!(f, "\n[{}]", self.data[0])?;
+        } else {
+            write!(f, "\n[")?;
+            for row in 0..self.rows {
+                for col in 0..self.cols {
+                    if col == 0 {
+                        write!(f, "[")?;
+                    } else {
+                        write!(f, ", ")?;
+                    }
+                    write!(f, "{}", self.data[row * self.cols + col])?;
                 }
-                write!(f, "{}", self.data[row * self.cols + col])?;
-            }
 
-            write!(f, "]")?;
-            if row < self.rows - 1 {
-                writeln!(f)?;
+                write!(f, "]")?;
+                if row < self.rows - 1 {
+                    writeln!(f)?;
+                }
             }
+            writeln!(f)?;
         }
-        writeln!(f)?;
 
         FmtResult::Ok(())
     }
@@ -126,6 +130,24 @@ where
         }
 
         true
+    }
+}
+
+impl<T> ops::Index<usize> for Matrix<T>
+where
+    T: Zero
+        + ops::Sub<Output = T>
+        + ops::Mul<Output = T>
+        + Sized
+        + Clone
+        + Copy
+        + Display
+        + PartialEq,
+{
+    type Output = [T];
+
+    fn index(&self, i: usize) -> &[T] {
+        &self.data[i * self.cols..i * self.cols + self.cols]
     }
 }
 
@@ -219,6 +241,262 @@ where
         Matrix {
             rows: self.rows,
             cols: _rhs.cols,
+            data,
+        }
+    }
+}
+
+pub struct SquareMatrix<T>
+where
+    T: Zero
+        + ops::Sub<Output = T>
+        + ops::Mul<Output = T>
+        + Sized
+        + Clone
+        + Copy
+        + Display
+        + PartialEq,
+{
+    pub dimension: usize,
+    data: Vec<T>,
+}
+
+impl<T> SquareMatrix<T>
+where
+    T: Zero
+        + ops::Sub<Output = T>
+        + ops::Mul<Output = T>
+        + Sized
+        + Clone
+        + Copy
+        + Display
+        + PartialEq,
+{
+    pub fn new(dimension: usize, data: Vec<Vec<T>>) -> SquareMatrix<T> {
+        assert_eq!(data.len(), dimension);
+        let mut new_data = vec![T::zero(); dimension * dimension];
+
+        for row in 0..data.len() {
+            assert_eq!(data[row].len(), dimension);
+            for col in 0..dimension {
+                new_data[row * dimension + col] = data[row][col];
+            }
+        }
+
+        SquareMatrix {
+            dimension,
+            data: new_data,
+        }
+    }
+
+    pub fn submatrix(&self, m: usize, n: usize) -> SquareMatrix<T> {
+        assert!(self.dimension.is_multiple_of(2));
+        let new_dimension = self.dimension / 2;
+        assert!(m < new_dimension || new_dimension == 1);
+        assert!(n < new_dimension || new_dimension == 1);
+
+        let mut data = vec![T::zero(); new_dimension * new_dimension];
+        for (p, row) in (0..new_dimension).zip(m * new_dimension..(m + 1) * new_dimension) {
+            for (q, col) in (0..new_dimension).zip(n * new_dimension..(n + 1) * new_dimension) {
+                data[p * new_dimension + q] = self.data[row * self.dimension + col];
+            }
+        }
+
+        SquareMatrix {
+            dimension: new_dimension,
+            data,
+        }
+    }
+}
+
+impl<T> Clone for SquareMatrix<T>
+where
+    T: Zero
+        + ops::Sub<Output = T>
+        + ops::Mul<Output = T>
+        + Sized
+        + Clone
+        + Copy
+        + Display
+        + PartialEq,
+{
+    fn clone(&self) -> SquareMatrix<T> {
+        SquareMatrix {
+            dimension: self.dimension,
+            data: self.data.clone(),
+        }
+    }
+}
+
+impl<T> Debug for SquareMatrix<T>
+where
+    T: Zero
+        + ops::Sub<Output = T>
+        + ops::Mul<Output = T>
+        + Sized
+        + Clone
+        + Copy
+        + Display
+        + PartialEq,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        if self.dimension == 1 {
+            writeln!(f, "\n[{}]", self.data[0])?;
+        } else {
+            write!(f, "\n[")?;
+
+            for row in 0..self.dimension {
+                for col in 0..self.dimension {
+                    if col == 0 {
+                        write!(f, "[")?;
+                    } else {
+                        write!(f, ", ")?;
+                    }
+                    write!(f, "{}", self.data[row * self.dimension + col])?;
+                }
+
+                write!(f, "]")?;
+                if row < self.dimension - 1 {
+                    writeln!(f)?;
+                }
+            }
+            writeln!(f)?;
+        }
+
+        FmtResult::Ok(())
+    }
+}
+
+impl<T> PartialEq for SquareMatrix<T>
+where
+    T: Zero
+        + ops::Sub<Output = T>
+        + ops::Mul<Output = T>
+        + Sized
+        + Clone
+        + Copy
+        + Display
+        + PartialEq,
+{
+    fn eq(&self, _rhs: &SquareMatrix<T>) -> bool {
+        if self.dimension != _rhs.dimension {
+            return false;
+        }
+
+        for i in 0..self.data.len() {
+            if self.data[i] != _rhs.data[i] {
+                return false;
+            }
+        }
+
+        true
+    }
+}
+
+impl<T> ops::Index<usize> for SquareMatrix<T>
+where
+    T: Zero
+        + ops::Sub<Output = T>
+        + ops::Mul<Output = T>
+        + Sized
+        + Clone
+        + Copy
+        + Display
+        + PartialEq,
+{
+    type Output = [T];
+
+    fn index(&self, i: usize) -> &[T] {
+        &self.data[i * self.dimension..i * self.dimension + self.dimension]
+    }
+}
+
+impl<T> ops::Add<SquareMatrix<T>> for SquareMatrix<T>
+where
+    T: Zero
+        + ops::Sub<Output = T>
+        + ops::Mul<Output = T>
+        + Sized
+        + Clone
+        + Copy
+        + Display
+        + PartialEq,
+{
+    type Output = SquareMatrix<T>;
+
+    fn add(self, _rhs: SquareMatrix<T>) -> SquareMatrix<T> {
+        assert_eq!(self.dimension, _rhs.dimension);
+
+        let mut data = vec![T::zero(); self.dimension * self.dimension];
+        for i in 0..self.data.len() {
+            data[i] = self.data[i] + _rhs.data[i];
+        }
+
+        SquareMatrix {
+            dimension: self.dimension,
+            data,
+        }
+    }
+}
+
+impl<T> ops::Sub<SquareMatrix<T>> for SquareMatrix<T>
+where
+    T: Zero
+        + ops::Sub<Output = T>
+        + ops::Mul<Output = T>
+        + Sized
+        + Clone
+        + Copy
+        + Display
+        + PartialEq,
+{
+    type Output = SquareMatrix<T>;
+
+    fn sub(self, _rhs: SquareMatrix<T>) -> SquareMatrix<T> {
+        assert_eq!(self.dimension, _rhs.dimension);
+
+        let mut data = vec![T::zero(); self.dimension * self.dimension];
+        for i in 0..self.data.len() {
+            data[i] = self.data[i] - _rhs.data[i];
+        }
+
+        SquareMatrix {
+            dimension: self.dimension,
+            data,
+        }
+    }
+}
+
+impl<T> ops::Mul<SquareMatrix<T>> for SquareMatrix<T>
+where
+    T: Zero
+        + ops::Sub<Output = T>
+        + ops::Mul<Output = T>
+        + Sized
+        + Clone
+        + Copy
+        + Display
+        + PartialEq,
+{
+    type Output = SquareMatrix<T>;
+
+    fn mul(self, _rhs: SquareMatrix<T>) -> SquareMatrix<T> {
+        assert_eq!(self.dimension, _rhs.dimension);
+
+        let mut data = vec![T::zero(); self.dimension * _rhs.dimension];
+        for i in 0..self.dimension {
+            for j in 0.._rhs.dimension {
+                let mut cij = T::zero();
+                for k in 0..self.dimension {
+                    cij = cij
+                        + (self.data[i * self.dimension + k] * _rhs.data[k * _rhs.dimension + j]);
+                }
+                data[i * _rhs.dimension + j] = cij;
+            }
+        }
+
+        SquareMatrix {
+            dimension: self.dimension,
             data,
         }
     }
