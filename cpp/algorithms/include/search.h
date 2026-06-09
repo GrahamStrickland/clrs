a214694f72a2c11386824332f80c6c7f1b09825d
Graham Strickland <grahamhstrickland@gmail.com>
2026-06-09 18:49:41 +0200

    Cut implementation files for template algorithms

--- cpp/algorithms/include/search.h
+++ cpp/algorithms/include/search.h
@@ -8,16 +8,54 @@ namespace algorithms {
 namespace search {
 template <typename T, std::size_t N, typename V>
 // Binary-Search algorithm from ex. 2.3-5 from p.39 of CLRS 3e
-int binary_search(std::span<T, N> a, V const &nu);
+int binary_search(std::span<T, N> a, V const &nu) {
+  std::size_t low = 0;
+  std::size_t high = a.size() - 1;
+
+  while (low <= high) {
+    std::size_t mid = low + (high - low) / 2;
+
+    if (nu == a[mid])
+      return mid;
+    else if (nu > a[mid])
+      low = mid + 1;
+    else
+      high = mid - 1;
+  }
+
+  return -1;
+}
 
 // Linear-Search algorithm from ex2.1-3 p.22 of CLRS 3e
 template <typename T, std::size_t N, typename V>
-int linear_search(std::span<T, N> a, V const &nu);
+int linear_search(std::span<T, N> a, V const &nu) {
+  std::size_t j = 0;
+
+  while (j != a.size()) {
+    if (a[j] == nu)
+      return j;
+    else
+      j += 1;
+  }
+
+  return -1;
+}
 
 // Recursive-Binary-Search algorithm from ex. 2.3-5 from p.39 of CLRS 3e
 template <typename T, std::size_t N, typename V>
 int recursive_binary_search(std::span<T, N> a, V const &nu,
-                          std::size_t const &low, std::size_t const &high);
+                          std::size_t const &low, std::size_t const &high) {
+  if (low > high)
+    return -1;
+  std::size_t mid = low + (high - low) / 2;
+  if (nu == a[mid])
+    return mid;
+  else if (nu > a[mid])
+    return recursive_binary_search(a, nu, mid + 1, high);
+  else
+    return recursive_binary_search(a, nu, low, mid - 1);
+}
+
 } // namespace search
 } // namespace algorithms
 } // namespace clrs
--- cpp/algorithms/include/sorting.h
+++ cpp/algorithms/include/sorting.h
@@ -2,35 +2,220 @@
 #define SORTING_H
 
 #include <array>
+#include <vector>
 
 namespace clrs {
 namespace algorithms {
 namespace sorting {
 // Bubble-Sort algorithm from p.2-2 on pg. 40 of CLRS 3e
-template <typename T, std::size_t N> void bubble_sort(std::array<T, N> &a);
+template <typename T, std::size_t N> void bubble_sort(std::array<T, N> &a) {
+  auto len = a.size();
+  for (std::size_t i = 0; i < len; i++) {
+    for (std::size_t j = len - 1; j >= i + 1; j--) {
+      if (a[j] < a[j - 1]) {
+        int32_t temp = a[j];
+        a[j] = a[j - 1];
+        a[j - 1] = temp;
+      }
+    }
+  }
+}
 
 // Count-Inversions algorithm from p. 2-4d on pg. 42 of CLRS 3e
 template <typename T, std::size_t N>
-std::size_t count_inversions(std::array<T, N> &a, std::size_t p, std::size_t r);
+std::size_t merge_inversions(std::array<T, N> &a, std::size_t p, std::size_t q,
+                             std::size_t r) {
+  std::size_t n1 = q - p + 1, n2 = r - q;
+
+  std::vector<T> larr;
+  larr.reserve(n1 + 1);
+  std::vector<T> rarr;
+  rarr.reserve(n2 + 1);
+
+  for (std::size_t i = 0; i < n1; i++)
+    larr[i] = a[p + i];
+  for (std::size_t j = 0; j < n2; j++)
+    rarr[j] = a[q + j + 1];
+
+  larr[n1] = INT_MAX;
+  rarr[n2] = INT_MAX;
+
+  std::size_t i = 0, j = 0, inversions = 0;
+  bool counted = false;
+
+  for (std::size_t k = p; k <= r; k++) {
+    if (!counted && rarr[j] < larr[i]) {
+      inversions += n1 - i;
+      counted = true;
+    }
+    if (larr[i] <= rarr[j]) {
+      a[k] = larr[i];
+      i++;
+    } else {
+      a[k] = rarr[j];
+      j++;
+      counted = false;
+    }
+  }
+
+  return inversions;
+}
+
+template <typename T, std::size_t N>
+std::size_t count_inversions(std::array<T, N> &a, std::size_t p,
+                             std::size_t r) {
+  std::size_t inversions = 0;
+  if (p < r) {
+    std::size_t q = (p + r) / 2;
+    inversions += count_inversions(a, p, q);
+    inversions += count_inversions(a, q + 1, r);
+    inversions += merge_inversions(a, p, q, r);
+  }
+
+  return inversions;
+}
 
 // Insertion-Sort algorithm from p. 18 of CLRS 3e
-template <typename T, std::size_t N> void insertion_sort(std::array<T, N> &a);
+template <typename T, std::size_t N> void insertion_sort(std::array<T, N> &a) {
+  auto len = a.size();
+  for (std::size_t j = 1; j < len; j++) {
+    T key = a[j];
+
+    // Insert A[j] into the sorted sequence A[1..j-1]
+    std::size_t i = j - 1;
+    while (i >= 0 && a[i] > key) {
+      T tmp = a[i + 1];
+      a[i + 1] = a[i];
+      a[i] = tmp;
+      i--;
+    }
+    a[i + 1] = key;
+  }
+}
 
 // Insertion-Sort algorithm in non-increasing order from ex2.1-2 on p.22 of CLRS
 // 3e
 template <typename T, std::size_t N>
-void insertion_sort_reverse(std::array<T, N> &a);
+void insertion_sort_reverse(std::array<T, N> &a) {
+  for (std::size_t j = 1; j < a.size(); j++) {
+    T key = a[j];
+
+    // Insert A[j] into the sorted sequence A[1..j-1]
+    auto i = j - 1;
+    while (a[i] < key) {
+      std::swap(a[i], a[i + 1]);
+      i = i > 0 ? i - 1 : 0;
+    }
+  }
+}
 
 // Merge-Sort algorithm from p.31-34 of CLRS 3e
 template <typename T, std::size_t N>
-void merge_sort(std::array<T, N> &a, std::size_t p, std::size_t r);
+void merge(std::array<T, N> &a, std::size_t p, std::size_t q, std::size_t r) {
+  std::size_t n1 = q - p + 1, n2 = r - q;
+
+  std::vector<T> larr;
+  larr.reserve(n1 + 1);
+  std::vector<T> rarr;
+  rarr.reserve(n2 + 1);
+
+  for (std::size_t i = 0; i < n1; i++)
+    larr[i] = a[p + i];
+  for (std::size_t j = 0; j < n2; j++)
+    rarr[j] = a[q + j + 1];
+
+  larr[n1] = INT_MAX;
+  rarr[n2] = INT_MAX;
+
+  std::size_t i = 0, j = 0;
+
+  for (std::size_t k = p; k <= r; k++) {
+    if (larr[i] <= rarr[j]) {
+      a[k] = larr[i];
+      i++;
+    } else {
+      a[k] = rarr[j];
+      j++;
+    }
+  }
+}
+
+template <typename T, std::size_t N>
+void merge_sort(std::array<T, N> &a, std::size_t p, std::size_t r) {
+  if (p < r) {
+    std::size_t q = (p + r) / 2;
+    merge_sort<T, N>(a, p, q);
+    merge_sort<T, N>(a, q + 1, r);
+    merge<T, N>(a, p, q, r);
+  }
+}
 
 // Merge-Sort algorithm without sentinel values from ex. 2.3-2 p.37 of CLRS 3e
 template <typename T, std::size_t N>
-void merge_sort_no_sentinel(std::array<T, N> &a, std::size_t p, std::size_t r);
+void merge_no_sentinel(std::array<T, N> &a, std::size_t p, std::size_t q,
+                       std::size_t r) {
+  std::size_t n1 = q - p + 1, n2 = r - q;
+
+  std::vector<T> larr;
+  larr.reserve(n1 + 1);
+  std::vector<T> rarr;
+  rarr.reserve(n2 + 1);
+
+  for (std::size_t i = 0; i < n1; i++)
+    larr[i] = a[p + i];
+  for (std::size_t j = 0; j < n2; j++)
+    rarr[j] = a[q + j + 1];
+
+  std::size_t i = 0, j = 0;
+
+  for (std::size_t k = p; k <= r; k++) {
+    if (i < n1 && j < n2) {
+      if (larr[i] <= rarr[j]) {
+        a[k] = larr[i];
+        i++;
+      } else {
+        a[k] = rarr[j];
+        j++;
+      }
+    } else {
+      if (i < n1) {
+        a[k] = larr[i];
+        i++;
+      } else if (j < n2) {
+        a[k] = rarr[j];
+        j++;
+      }
+    }
+  }
+}
+
+template <typename T, std::size_t N>
+void merge_sort_no_sentinel(std::array<T, N> &a, std::size_t p, std::size_t r) {
+  if (p < r) {
+    uint8_t q = (p + r) / 2;
+    merge_sort_no_sentinel<T, N>(a, p, q);
+    merge_sort_no_sentinel<T, N>(a, q + 1, r);
+    merge_no_sentinel<T, N>(a, p, q, r);
+  }
+}
 
 // Selection-Sort algorithm from ex2.2-2 p.29 of CLRS 3e
-template <typename T, std::size_t N> void selection_sort(std::array<T, N> &a);
+template <typename T, std::size_t N> void selection_sort(std::array<T, N> &a) {
+  std::size_t smallest;
+  auto len = a.size();
+
+  for (std::size_t i = 0; i < len - 1; i++) {
+    smallest = i;
+
+    for (std::size_t j = i + 1; j < len; j++)
+      if (a[j] < a[smallest])
+        smallest = j;
+
+    T tmp = a[i];
+    a[i] = a[smallest];
+    a[smallest] = tmp;
+  }
+}
 } // namespace sorting
 } // namespace algorithms
 } // namespace clrs
